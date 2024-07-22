8ba7c69d85d0965d862f65a5e279fc6785f8eb42
cpbryant <113325662+cpbryant@users.noreply.github.com>
2024-07-22 17:24:54 -0400

    Made homepage look good

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,7 +1,7 @@
-// src/pages/HomePage.js
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { checkAuthentication, logoutUser } from './auth';
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { checkAuthentication, logoutUser } from "./auth";
+import "./HomePage.css"; // Import the CSS file
 
 const HomePage = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -21,12 +21,13 @@ const HomePage = () => {
     const success = await logoutUser();
     if (success) {
       setIsAuthenticated(false);
-      navigate('/');
+      navigate("/");
     }
   };
 
   return (
     <div className="home-page">
+      <h1 className="welcome-message">Welcome to your Vacation Finder!</h1>
       <div className="header-content">
         <Link to="/hotel">
           <button className="btn btn-secondary">Search Hotels</button>
@@ -35,10 +36,17 @@ const HomePage = () => {
           <button className="btn btn-secondary">Search Flights</button>
         </Link>
         {isAuthenticated && (
-          <button className="btn btn-secondary" onClick={() => navigate('/profile')}>My Profile</button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => navigate("/profile")}
+          >
+            My Profile
+          </button>
         )}
         {isAuthenticated ? (
-          <button className="btn btn-secondary" onClick={handleLogout}>Logout</button>
+          <button className="btn btn-secondary" onClick={handleLogout}>
+            Logout
+          </button>
         ) : (
           <>
             <Link to="/login">
